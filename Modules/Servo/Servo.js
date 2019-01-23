043173293a169f3a26bf42beec49de10318a4d45
Trevor Gionet <tjgionet@gmail.com>
2019-01-23 09:15:32 -0500

    creating pwm control

---
+++ Modules/Servo/Servo.js
@@ -0,0 +1,78 @@
+class Servo {
+
+	Setup(com, fun) {
+		log.i('Servo::Setup 50Hz on GPIO${this.Par.Pin}');
+		const Gpio = require('gpio');
+
+		this.Vlt.Servo = new Gpio(this.Par.Pin, { mode: Gpio.OUTPUT });
+
+		this.Vlt.Width = this.Par.CENTER_PULSEWIDTH;
+		fun(null, com);
+	}
+
+	Start(com, fun) {
+		if (com.Width < this.Par.MIN_PULSEWIDTH || com.Width > this.Par.MAX_PULSEWIDTH) {
+			return fun(`Servo::Start error GPIO:${this.Par.Pin}, Width ${com.Width} out of Range`, com);
+		}
+
+		this.Vlt.Width = com.Width;
+		this.Vlt.Servo.servoWrite(com.Width);
+		fun(null, com);
+	}
+
+	SetWidth(com, fun) {
+		if (com.Width < this.Par.MIN_PULSEWIDTH || com.Width > this.Par.MAX_PULSEWIDTH) {
+			return fun(`Servo::SetWidth error GPIO:${this.Par.Pin}, Width ${com.Width} out of Range`, com);
+		}
+
+		this.Vlt.Width = com.Width;
+		this.Vlt.Servo.servoWrite(com.Width);
+		fun(null, com);
+	}
+
+	async Slide(com, fun) {
+		if (com.Width < this.Par.MIN_PULSEWIDTH || com.Width > this.Par.MAX_PULSEWIDTH) {
+			return fun(`Servo::Slide error GPIO:${this.Par.Pin}, Width ${com.Width} out of Range`, com);
+		}
+
+		let startWidth = this.Vlt.Width;
+		let stepSize = 0;
+
+		if ('Steps' in com) {
+			stepSize = Math.round((com.Width - startWidth) / com.Steps);
+		}
+
+		if ('Wait' in com) {
+			if (com.Wait < 100) stepSize = com.Width - startWidth;
+			else {
+				let minsteptime = 100;
+				let nSteps = com.Wait / minsteptime;
+				stepSize = Math.round((com.Width - startWidth) / nSteps);
+			}
+		}
+
+		for (let width = startWidth; Math.abs(width - startWidth > stepSize); width += stepSize) {
+			if (width < this.Par.MIN_PULSEWIDTH || width > this.Par.MAX_PULSEWIDTH) {
+				return fun(`Servo::Slide error GPIO:${this.Par.Pin}, Width ${com.Width} out of Range`, com);
+			}
+			this.Vlt.Width = Math.round(width);
+			this.Vlt.Servo.servoWrite(this.Vlt.Width);
+			await new Promise((res) => setTimeout(res, Math.round(com.StepWait) || 100));
+		}
+
+		this.Vlt.Width = Math.round(com.Width);
+		this.Vlt.Servo.servoWrite(this.Vlt.Width);
+
+		fun(null, com);
+	}
+
+	SetMax(com, fun) {
+		this.Par.MAX_PULSEWIDTH = com.Value;
+		fun(null, com);
+	}
+
+	SetMin(com, fun) {
+		this.Par.MIN_PULSEWIDTH = com.Value;
+		fun(null, com);
+	}
+}
